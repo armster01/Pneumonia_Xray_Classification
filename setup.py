d816dbea933f0b1446ed8326d8a9cfec3bf19d7c
armster01 <armstersaha8@gmail.com>
2026-02-02 23:52:09 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Xray',
+    version= '0.0.1',
+    author= 'Adittya Saha',
+    author_email= 'adittyasaha261102@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+
+)
